025d4fcbd34dba82949f13b14832d25f94f4d38f
Zach Dodson <zdodson21@gmail.com>
2025-03-11 11:45:38 -0400

    rearrange files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TaskbarBaseComponent } from "./components/taskbar-base/taskbar-base.component";
+import { TaskbarBaseComponent } from "../components/taskbar-base/taskbar-base.component";
 
 @Component({
   selector: 'app-root',
