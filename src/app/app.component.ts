a9b9fefe044f99bea6a9ca6205ebfeb98a312ddd
Zach Dodson <zdodson21@gmail.com>
2025-03-12 16:56:18 -0400

    style refactoring complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { TaskbarBaseComponent } from "../components/taskbar-base/taskbar-base.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TaskbarBaseComponent],
+  imports: [TaskbarBaseComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
