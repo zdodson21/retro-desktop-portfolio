355823ac626ba2725cf9bc076679103b565f89fa
Zach Dodson <zdodson21@gmail.com>
2025-03-15 13:09:20 -0400

    vercel analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 import { TaskbarBaseComponent } from "../components/taskbar/taskbar-base/taskbar-base.component";
 import { DesktopEnvironmentComponent } from '../components/desktop/desktop-environment/desktop-environment.component';
+import { inject } from '@vercel/analytics';
+import { injectSpeedInsights} from '@vercel/speed-insights'
 
+inject();
+injectSpeedInsights();
 
 @Component({
   selector: 'app-root',
