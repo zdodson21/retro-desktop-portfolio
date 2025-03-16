97507a133264911f584d46481d8e8b3aa109b266
Zach Dodson <zdodson21@gmail.com>
2025-03-16 16:55:53 -0400

    file rearrangement

--- src/components/desktop/desktop-environment/desktop-environment.component.ts
+++ src/components/desktop/desktop-environment/desktop-environment.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { DesktopIconComponent } from '../desktop-icon/desktop-icon.component';
-import { WindowFrameComponent } from '../../windows/window-frame/window-frame.component';
+import { WindowFrameComponent } from '../../window-frame/window-frame.component';
 
 @Component({
   selector: 'desktop-environment',
---
+++ src/components/start/start-menu/start-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-menu',
+  imports: [],
+  templateUrl: './start-menu.component.html',
+  styleUrl: './start-menu.component.scss'
+})
+export class StartMenuComponent {
+
+}
