159d605d7f74d9f9ecfb4940c51ec82111f37a30
Zach Dodson <zdodson21@gmail.com>
2025-04-12 15:01:56 -0400

    test configuration

---
+++ karma.conf.js
@@ -0,0 +1,27 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false,
+      jasmine: {
+        random: false,
+        seed: null
+      }
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true,
+      random: false
+    },
+    reporters: ['progress', 'kjhtml'],
+    browsers: ['Chrome'],
+    restartOnFileChange: true
+  });
+};
