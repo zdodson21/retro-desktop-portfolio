3647364f5c33a15679c6affdb35ac1abab208a0e
Zach Dodson <zdodson21@gmail.com>
2025-09-02 16:02:32 -0400

    testing working with analytics moved. will keep environments for now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,12 @@ import {
   signal,
   WritableSignal,
 } from '@angular/core';
-import { inject as vercelAnalytics } from '@vercel/analytics';
-import { injectSpeedInsights as vercelSpeedInsights } from '@vercel/speed-insights';
 import { AlertShutdownComponent } from '../components/alerts/alert-shutdown/alert-shutdown.component';
 import { DesktopEnvironmentComponent } from '../components/desktop/desktop-environment/desktop-environment.component';
 import { SystemMonitorComponent } from '../components/programs/system-monitor/system-monitor.component';
 import { StartMenuComponent } from '../components/start/start-menu/start-menu.component';
 import { TaskbarBaseComponent } from '../components/taskbar/taskbar-base/taskbar-base.component';
 import { AppService } from './app.service';
-import { environment } from '../environments/environment.development';
-
-if (environment.production) {
-  // TODO these might need fixed, should monitor. This is needed for testing though.
-  // Mainly just need to monitor if it is being build properly for production.
-  // Look into how to make sure angular and vercel knows to build in prod
-  vercelAnalytics();
-  vercelSpeedInsights();
-}
 
 @Component({
   selector: 'app-root',
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { inject as vercelAnalytics } from '@vercel/analytics';
+import { injectSpeedInsights as vercelSpeedInsights } from '@vercel/speed-insights';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err),
 );
+
+vercelAnalytics();
+vercelSpeedInsights();
