60a6dbad4936608d559998d4b4850eeaaa9e3be2
Zach Dodson <zdodson21@gmail.com>
2025-09-16 10:08:45 -0400

    small changes

--- src/components/programs/welcome/welcome.component.ts
+++ src/components/programs/welcome/welcome.component.ts
@@ -9,9 +9,9 @@ import { WindowFrameComponent } from '../../window-frame/window-frame.component'
 })
 export class WelcomeComponent {
   public tips: Array<string> = [
-    // ! this array must have at least 2 values, or page will crash when randomizing
     "If you don't know how to find something, you can look for directions in Help. Just click the Start button, and then click Help.",
     'This web application functions similarly to many desktop computer interfaces. You should be able to navigate this web application similar to how you would navigate many desktop computers.',
+    'A good place to start is the about me page in Internet Explorer. Just click the Start button, and then click Internet Explorer. You will be taken to the About Me page by default. You can also find the "about-me.html" file in Windows Explorer.',
     "You can reset this web application's local storage values in Control Panel. Click the Start button, and then hover over Settings, and then click Control Panel. You will find a button to reset to the initial default state there.",
     'The Shut Down command in the Shut Down menu will take you to my GitHub profile (due to browser security blocking the capability to close browser tabs with JavaScript / TypeScript).',
     'You can open Internet Explorer to view my "About Me" and Project pages. Internet Explorer can be accessed from the Desktop or Start Menu. These pages can also be accessed by locating and double clicking them within Windows Explorer.',
