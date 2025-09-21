e373b4723ed72dcb46625056464776a811c402a6
Zach Dodson <zdodson21@gmail.com>
2025-09-21 19:50:14 -0400

    My Computer content complete

--- src/components/programs/my-computer/my-computer.component.ts
+++ src/components/programs/my-computer/my-computer.component.ts
@@ -7,4 +7,37 @@ import { WindowFrameComponent } from '../../window-frame/window-frame.component'
   templateUrl: './my-computer.component.html',
   styleUrl: './my-computer.component.scss',
 })
-export class MyComputerComponent {}
+export class MyComputerComponent {
+  public availableCores: number = navigator.hardwareConcurrency;
+  private userAgent: string = navigator.userAgent;
+
+  public getBrowser(): string {
+    if (this.userAgent.includes('Edg/')) {
+      return 'Microsoft Edge';
+    } else if (this.userAgent.includes('OPR/')) {
+      return 'Opera';
+    } else if (this.userAgent.includes('Firefox')) {
+      return 'Firefox';
+    } else if (this.userAgent.includes('Chrome')) {
+      return 'Google Chrome';
+    } else if (this.userAgent.includes('Safari')) {
+      return 'Safari / Webkit Browser';
+    }
+
+    return 'Unidentified Browser';
+  }
+
+  public getOS(): string {
+    if (this.userAgent.includes('Linux')) {
+      return 'Linux';
+    } else if (this.userAgent.includes('Windows')) {
+      return 'Windows';
+    } else if (this.userAgent.includes('Macintosh')) {
+      return 'macOS';
+    } else if (this.userAgent.includes('FreeBSD')) {
+      return 'FreeBSD';
+    }
+
+    return 'Unidentified Operating System';
+  }
+}
