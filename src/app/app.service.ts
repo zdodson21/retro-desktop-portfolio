398beb317426393424167fe70daec2f31c5b7533
Zach Dodson <zdodson21@gmail.com>
2025-09-21 11:20:20 -0400

    removed TODOs

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -10,7 +10,6 @@ export class AppService {
   // ! Common
   focus: WritableSignal<string> = signal('');
   openPrograms: OpenPrograms = signal([]);
-  // TODO When a program is added to openPrograms, it needs to be added to the route params so it can render
 
   // ! Start Menu
 
@@ -23,8 +22,7 @@ export class AppService {
   viewportHeight: WritableSignal<number> = signal(globalThis.innerHeight);
 
   // ! Functions
-  // TODO add function for if programs query is empty, then programs query is removed.
-  // TODO might need to add a similar function for internet explorer
+
 
   // ! Future Items
   isSoundEnabled: WritableSignal<boolean> = signal(false);
