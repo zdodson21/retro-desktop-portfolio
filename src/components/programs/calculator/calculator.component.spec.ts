152d43d5499dbafd0c9771e03dd177b34b989377
Zach Dodson <zdodson21@gmail.com>
2025-09-21 15:22:20 -0400

    calculator start

---
+++ src/components/programs/calculator/calculator.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { CalculatorComponent } from './calculator.component';
+
+describe('CalculatorComponent', () => {
+  let component: CalculatorComponent;
+  let fixture: ComponentFixture<CalculatorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CalculatorComponent],
+      providers: [provideRouter([])],
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CalculatorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/components/programs/calculator/calculator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { WindowFrameComponent } from '../../window-frame/window-frame.component';
+
+@Component({
+  selector: 'app-calculator',
+  imports: [WindowFrameComponent],
+  templateUrl: './calculator.component.html',
+  styleUrl: './calculator.component.scss'
+})
+export class CalculatorComponent {
+
+}
