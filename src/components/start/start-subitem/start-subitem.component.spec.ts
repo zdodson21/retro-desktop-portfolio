d319b3d3be736f8f58c6922cc58f9ea6da8edda9
Zach Dodson <zdodson21@gmail.com>
2025-10-09 18:35:16 -0400

    fix testing

--- src/components/start/start-subitem/start-subitem.component.spec.ts
+++ src/components/start/start-subitem/start-subitem.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideRouter } from '@angular/router';
 import { StartSubitemComponent } from './start-subitem.component';
 
 describe('StartSubitemComponent', () => {
@@ -8,7 +8,8 @@ describe('StartSubitemComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [StartSubitemComponent]
+      imports: [StartSubitemComponent],
+      providers: [provideRouter([])],
     })
     .compileComponents();
 
