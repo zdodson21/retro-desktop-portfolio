cba063c832e6eec19be8cd38e7ef064d67d6acdc
Zach Dodson <zdodson21@gmail.com>
2025-10-09 18:22:39 -0400

    menu layout complete, needs styles

--- src/components/start/start-subdropout/start-subdropout.component.ts
+++ src/components/start/start-subdropout/start-subdropout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'start-subdropout',
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './start-subdropout.component.scss'
 })
 export class StartSubdropoutComponent {
+  @Input({ alias: 'icon' }) public src: string;
+  @Input({ alias: 'text' }) public text: string;
+  @Input({ alias: 'bottom' }) public bottom: boolean;
 
+  protected isHovered: boolean = false;
 }
