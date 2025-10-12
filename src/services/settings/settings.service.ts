248fedaad11d0c3113951f55e1e89c178d15ca58
Zach Dodson <zdodson21@gmail.com>
2025-10-12 17:41:21 -0400

    update documentation

--- src/services/settings/settings.service.ts
+++ src/services/settings/settings.service.ts
@@ -8,6 +8,13 @@ export class SettingsService {
   public customBackgroundColor: WritableSignal<string> = signal('');
 
   public localStorageValues: Array<string> = [
+    /*
+      Display "Welcome" program on web application start up.
+
+      Possible values:
+        * "yes": displays welcome program on start up | (set by default on first page visit).
+        * "no": prevents display of welcome program on start up.
+    */
     "openWelcomeOnStartup", // 0
   ]
 }
