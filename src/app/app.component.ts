5e9c768d7bfd95634b804b3992c62a558195bd2f
Zach Dodson <zdodson21@gmail.com>
2025-10-12 14:50:33 -0400

    welcome opening with no localStorage value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component, HostListener, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, effect, HostListener, inject } from '@angular/core';
+import { ActivatedRoute, Params, Router, RouterOutlet } from '@angular/router';
 import { AlertShutdownComponent } from '../components/alerts/alert-shutdown/alert-shutdown.component';
 import { DesktopEnvironmentComponent } from '../components/desktop/desktop-environment/desktop-environment.component';
 import { ShutdownScreenComponent } from '../components/desktop/shutdown-screen/shutdown-screen.component';
+import { MsdosPromptComponent } from '../components/programs/msdos-prompt/msdos-prompt.component';
 import { StartMenuComponent } from '../components/start/start-menu/start-menu.component';
 import { TaskbarBaseComponent } from '../components/taskbar/taskbar-base/taskbar-base.component';
-import { AppService } from './app.service';
-import { MsdosPromptComponent } from '../components/programs/msdos-prompt/msdos-prompt.component';
 import { SystemService } from '../services/system/system.service';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
@@ -26,6 +26,23 @@ import { SystemService } from '../services/system/system.service';
 export class AppComponent {
   protected store: AppService = inject(AppService);
   private systemService: SystemService = inject(SystemService);
+
+  private router: Router = inject(Router);
+  private route: ActivatedRoute = inject(ActivatedRoute);
+
+  constructor() {
+    let openWelcome: string | null = localStorage.getItem("openWelcomeOnStart");
+    setTimeout(() => {
+      const CURRENT_PARAMS: Params = { ...this.route.snapshot.queryParams }
+      if (Object.keys(CURRENT_PARAMS).length === 0 && (openWelcome === null || openWelcome === "yes")) {
+        this.router.navigate(['programs'], {
+          relativeTo: this.route,
+          queryParams: {welcome: ''},
+          replaceUrl: true,
+        });
+      }
+    }, 1000);
+  }
 
   /**
    * @description Sets viewport dimension variables for live
