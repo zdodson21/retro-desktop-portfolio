2f9237915351080fb6df57ad9f4c9c140d9b8f91
Zach Dodson <zdodson21@gmail.com>
2025-10-14 17:03:30 -0400

    update site for initial calculator description

--- src/components/sites/retro-desktop-portfolio/retro-desktop-portfolio.component.ts
+++ src/components/sites/retro-desktop-portfolio/retro-desktop-portfolio.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { AIeComponent } from '../components/a-ie/a-ie.component';
+import { SystemService } from '../../../services/system/system.service';
 
 @Component({
   selector: 'retro-desktop-portfolio',
@@ -7,4 +8,6 @@ import { AIeComponent } from '../components/a-ie/a-ie.component';
   templateUrl: './retro-desktop-portfolio.component.html',
   styleUrl: './retro-desktop-portfolio.component.scss',
 })
-export class RetroDesktopPortfolioSite {}
+export class RetroDesktopPortfolioSite {
+  protected systemService: SystemService = inject(SystemService);
+}
