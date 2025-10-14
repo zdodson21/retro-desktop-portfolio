79aede2fed878590eac22349ac162ab910a39c2e
Zach Dodson <zdodson21@gmail.com>
2025-10-14 14:38:28 -0400

    helper functions

--- src/components/programs/calculator/calculator_test.c
+++ src/components/programs/calculator/calculator_test.c
@@ -2,6 +2,22 @@
 #include <stdbool.h>
 #include "calculator.h"
 
+// TODO figure out colored text (green & red) for pass and fail respectivly
+// TODO test moving any support (basically not directly testing functions) into their own C and H files.
+// TODO might also want to just rearrange file structure, have C test file in its own test directory along with support files
+
+void print_Bar(void) {
+  printf("-------------------\n");
+}
+
+char * convertResult(bool result) {
+  if (result == true) {
+    return "Pass";
+  }
+
+  return "Fail";
+}
+
 bool test_Addition(void) {
   bool test1, test2, test3;
   test1 = test2 = test3 = true;
@@ -10,15 +26,35 @@ bool test_Addition(void) {
     test1 = false;
   }
 
-  printf("Add 2 + 2: " + test1);
+  printf("Add 2 + 2: %s\n", convertResult(test1));
+
+  if (add(3, 2) != 5) {
+    test2 = false;
+  }
 
   if (test1 == false || test2 == false || test3 == false) {
     return false;
   }
-} 
+}
+
+bool test_Subtraction(void) {
+  bool test1, test2, test3;
+  test1 = test2 = test3 = true;
+
+  if (subtract(3, 2) != 1) {
+    test1 = false;
+  }
+
+  printf("Subtract 3 - 2: %s\n", convertResult(test1));
+}
 
 int main(void) {
-  printf("Running tests\n");
+  printf("Running tests:\n");
+  print_Bar();
   test_Addition();
+  print_Bar();
+  test_Subtraction();
+  print_Bar();
+  
   return 0;
 }
