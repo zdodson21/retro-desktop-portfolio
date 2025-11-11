b98f436caabee8fd2e11b372a38c14ff8397434d
Zach Dodson <zdodson21@gmail.com>
2025-11-11 14:06:53 -0500

    calculator program testing improvements

--- src/components/programs/calculator/calculator.component.spec.ts
+++ src/components/programs/calculator/calculator.component.spec.ts
@@ -1,11 +1,12 @@
+import { provideHttpClient } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
 import { CalculatorComponent } from './calculator.component';
-import { provideHttpClient } from '@angular/common/http';
 
 describe('CalculatorComponent', () => {
   let component: CalculatorComponent;
   let fixture: ComponentFixture<CalculatorComponent>;
+  let compiled: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,12 +16,66 @@ describe('CalculatorComponent', () => {
 
     fixture = TestBed.createComponent(CalculatorComponent);
     component = fixture.componentInstance;
+    compiled = fixture.nativeElement as HTMLElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('must be wrapped', () => {
+    expect(compiled.querySelector('.calculator-wrapper')).toBeTruthy();
+  });
+
+  describe('Structure', () => {
+    describe('Window Frame', () => {
+      it('window-title should be "Calculator"', () => {
+        expect(compiled.querySelector('window-frame')?.getAttribute('window-title')).toBe('Calculator');
+      });
+
+      it('focus-name should be "calculator"', () => {
+        expect(compiled.querySelector('window-frame')?.getAttribute('focus-name')).toBe('calculator');
+      })
+
+      it('must have proper icon path', () => {
+        expect(compiled.querySelector('window-frame')?.getAttribute('window-icon')).toBe('assets/icons/calculator.svg');
+      })
+    });
+
+    it('must have a frame-contents wrapper', () => {
+      expect(compiled.querySelector('window-frame .frame-contents')).toBeTruthy();
+    })
+
+    it('must have a toolbar', () => {
+      expect(compiled.querySelector('.toolbar')).toBeTruthy();
+    })
+
+    it('must have calculator section', () => {
+      expect(compiled.querySelector('.calculator')).toBeTruthy();
+    })
+
+    it('must display standard button set by default', () => {
+      expect(compiled.querySelector('.standard-button-set')).toBeTruthy();
+    })
+
+    it('must not display scientific button set by default', () => {
+      expect(compiled.querySelector('.scientific-button-set')).toBeFalsy();
+    })
+  });
+
+  // describe('button labels', () => {
+  //   const STD_LABELS = [];
+
+  //   it('must contain standard button labels', () => {
+       // TODO test forEach calculator button expect one of them to have the appropriate label
+  //   })
+
+  //    const SCI_LABELS = [];
+  //    it('must contain scientific & standard button labels', () => {
+  //
+  //    })
+  // })
 
   // TODO if possible, I would actually like to call WASM functions here for testing. Can use same tests as in C testing program.
 });
--- src/components/programs/calculator/components/calculator-button/calculator-button.component.spec.ts
+++ src/components/programs/calculator/components/calculator-button/calculator-button.component.spec.ts
@@ -5,6 +5,7 @@ import { CalculatorButtonComponent } from './calculator-button.component';
 describe('CalculatorButtonComponent', () => {
   let component: CalculatorButtonComponent;
   let fixture: ComponentFixture<CalculatorButtonComponent>;
+  let compiled: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -13,10 +14,19 @@ describe('CalculatorButtonComponent', () => {
 
     fixture = TestBed.createComponent(CalculatorButtonComponent);
     component = fixture.componentInstance;
+    compiled = fixture.nativeElement as HTMLElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should be wrapped', () => {
+    expect(compiled.querySelector('.calculator-button-wrapper')).toBeTruthy();
+  })
+
+  it('should have space for button text label', () => {
+    expect(compiled.querySelector('p')).toBeTruthy();
+  })
 });
