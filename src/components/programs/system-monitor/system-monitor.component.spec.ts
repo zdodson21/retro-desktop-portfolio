da74c6fdc8df81c22e2f226111384fe718cae068
Zach Dodson <zdodson21@gmail.com>
2025-11-11 18:57:09 -0500

    system monitor testing

--- src/components/programs/system-monitor/system-monitor.component.spec.ts
+++ src/components/programs/system-monitor/system-monitor.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { SystemMonitorComponent } from './system-monitor.component';
 import { provideRouter } from '@angular/router';
+import { SystemMonitorComponent } from './system-monitor.component';
 
 describe('SystemMonitorComponent', () => {
   let component: SystemMonitorComponent;
@@ -23,17 +23,27 @@ describe('SystemMonitorComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  describe('Window Management', () => {
-    it('focus-name should be "system-monitor"', () => {
-      expect(compiled.querySelector('window-frame')?.getAttribute('focus-name')).toBe('system-monitor');
+  it('should be wrapped', () => {
+    expect(compiled.querySelector('.system-monitor-wrapper')).toBeTruthy();
+  });
+
+  describe('Structure', () => {
+    describe('Window Frame', () => {
+      it('focus-name should be "system-monitor"', () => {
+        expect(compiled.querySelector('window-frame')?.getAttribute('focus-name')).toBe('system-monitor');
+      });
+
+      it('window-title should be "System Monitor"', () => {
+        expect(compiled.querySelector('window-frame')?.getAttribute('window-title')).toBe('System Monitor');
+      });
+
+      it('icon path should be correct', () => {
+        expect(compiled.querySelector('window-frame')?.getAttribute('window-icon')).toBe('assets/icons/system-monitor.svg');
+      });
     });
 
-    it('window-title should be "System Monitor"', () => {
-      expect(compiled.querySelector('window-frame')?.getAttribute('window-title')).toBe('System Monitor');
-    });
-
-    it('icon path should be correct', () => {
-      expect(compiled.querySelector('window-frame')?.getAttribute('window-icon')).toBe('assets/icons/system-monitor.svg');
-    });
+    it('should have frame-contents container', () => {
+      expect(compiled.querySelector('window-frame .frame-contents')).toBeTruthy();
+    })
   });
 });
