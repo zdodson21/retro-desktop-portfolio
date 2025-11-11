9a686e0dde80f137d919d22ff8b1ac0725f58978
Zach Dodson <zdodson21@gmail.com>
2025-11-11 14:07:10 -0500

    control panel program testing improvements

--- src/components/programs/control-panel/control-panel.component.spec.ts
+++ src/components/programs/control-panel/control-panel.component.spec.ts
@@ -5,6 +5,7 @@ import { ControlPanelComponent } from './control-panel.component';
 describe('ControlPanelComponent', () => {
   let component: ControlPanelComponent;
   let fixture: ComponentFixture<ControlPanelComponent>;
+  let compiled: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,10 +15,35 @@ describe('ControlPanelComponent', () => {
 
     fixture = TestBed.createComponent(ControlPanelComponent);
     component = fixture.componentInstance;
+    compiled = fixture.nativeElement as HTMLElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('must be wrapped', () => {
+    expect(compiled.querySelector('.control-panel-wrapper')).toBeTruthy();
+  });
+
+  describe('Structure', () => {
+    describe('window frame', () => {
+      it('must have window-title="Control Panel"', () => {
+        expect(compiled.querySelector('window-frame')?.getAttribute('window-title')).toBe('Control Panel');
+      });
+
+      it('must have proper focus name', () => {
+        expect(compiled.querySelector('window-frame')?.getAttribute('focus-name')).toBe('control-panel');
+      });
+
+      it('must have proper icon path', () => {
+        expect(compiled.querySelector('window-frame')?.getAttribute('window-icon')).toBe('assets/icons/control-panel.svg');
+      });
+    });
+
+    it('must have frame-contents wrapper', () => {
+      expect(compiled.querySelector('window-frame .frame-contents')).toBeTruthy();
+    })
   });
 });
