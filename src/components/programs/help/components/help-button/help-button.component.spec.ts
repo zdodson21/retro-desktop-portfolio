4c60d256caebe6a2762e084a249f33eff1e37118
Zach Dodson <zdodson21@gmail.com>
2025-11-11 14:07:29 -0500

    help program testing improvements

--- src/components/programs/help/components/help-button/help-button.component.spec.ts
+++ src/components/programs/help/components/help-button/help-button.component.spec.ts
@@ -5,6 +5,7 @@ import { HelpButtonComponent } from './help-button.component';
 describe('HelpButtonComponent', () => {
   let component: HelpButtonComponent;
   let fixture: ComponentFixture<HelpButtonComponent>;
+  let compiled: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -13,10 +14,38 @@ describe('HelpButtonComponent', () => {
 
     fixture = TestBed.createComponent(HelpButtonComponent);
     component = fixture.componentInstance;
+    compiled = fixture.nativeElement as HTMLElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('must be wrapped', () => {
+    expect(compiled.querySelector('.help-button-wrapper')).toBeTruthy();
+  });
+
+  describe('Structure', () => {
+    describe('Square', () => {
+      it('must exist', () => {
+        expect(compiled.querySelector('.square')).toBeTruthy();
+      })
+
+      it('must not be active', () => {
+        expect(compiled.querySelector('.square.active')).toBeFalsy();
+        expect(compiled.querySelector('.inner-square.active')).toBeFalsy();
+      })
+    })
+
+    describe('Text', () => {
+      it('must exist', () => {
+        expect(compiled.querySelector('p')).toBeTruthy();
+      })
+
+      it('must not be active by default', () => {
+        expect(compiled.querySelector('p.active')).toBeFalsy();
+      })
+    })
   });
 });
--- src/components/programs/help/help.component.spec.ts
+++ src/components/programs/help/help.component.spec.ts
@@ -5,6 +5,7 @@ import { HelpComponent } from './help.component';
 describe('WindowsHelpComponent', () => {
   let component: HelpComponent;
   let fixture: ComponentFixture<HelpComponent>;
+  let compiled: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,10 +15,35 @@ describe('WindowsHelpComponent', () => {
 
     fixture = TestBed.createComponent(HelpComponent);
     component = fixture.componentInstance;
+    compiled = fixture.nativeElement as HTMLElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should be wrapped', () => {
+    expect(compiled.querySelector('.windows-help-wrapper')).toBeTruthy();
+  })
+
+  describe('Structure', () => {
+    describe('Window Frame', () => {
+      it('should have proper window title', () => {
+        expect(compiled.querySelector('window-frame')?.getAttribute('window-title')).toBe('Windows Help');
+      });
+
+      it('should have proper focus name', () => {
+        expect(compiled.querySelector('window-frame')?.getAttribute('focus-name')).toBe('help');
+      });
+
+      it('should have proper window icon', () =>{
+        expect(compiled.querySelector('window-frame')?.getAttribute('window-icon')).toBe('assets/icons/windows-help.svg');
+      })
+    })
+
+    it('should have frame contents wrapper', () => {
+      expect(compiled.querySelector('window-frame .frame-contents')).toBeTruthy();
+    })
+  })
 });
