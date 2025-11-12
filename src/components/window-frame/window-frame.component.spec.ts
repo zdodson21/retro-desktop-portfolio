f58db912fd1f9f8d34b0983e3fe1dece557a7b19
Zach Dodson <zdodson21@gmail.com>
2025-11-11 21:21:10 -0500

    window frame basic testing

--- src/components/window-frame/window-frame.component.spec.ts
+++ src/components/window-frame/window-frame.component.spec.ts
@@ -29,7 +29,7 @@ describe('WindowFrameComponent', () => {
 
   describe('top control panel', () => {
     it('must exist', () => {
-      expect(compiled.querySelector('.top-panel'));
+      expect(compiled.querySelector('.top-panel')).toBeTruthy();
     });
 
     describe('left-side', () => {
@@ -37,68 +37,59 @@ describe('WindowFrameComponent', () => {
         expect(compiled.querySelector('.top-panel .left-side')).toBeTruthy();
       });
 
-      // test stuff for img tag
-
       it('must not have img without icon set', () => {
         expect(compiled.querySelector('.top-panel .left-side img')).toBeFalsy();
-      })
+      });
+
+      // TODO test for img tag when icon var is set
+
+      it('must have space for text', () => {
+        expect(compiled.querySelector('.top-panel .left-side p')).toBeTruthy();
+      });
+
+      // TODO test for innerHTML of p tag when title var is set
     });
 
     describe('Buttons', () => {
-      describe('Assets', () => {
-        // TODO add test for minimize button when icon added
+      it('must be wrapped', () => {
+        expect(compiled.querySelector('.controls')).toBeTruthy();
+      })
+
+      describe('minimize', () => {
+        it('should exist by default', () => {
+          expect(compiled.querySelector('.minimize-button')).toBeTruthy();
+        });
+
+        // TODO test not existing when hideButtons set to true
+
         it('should have proper minimize button icon', () => {
           expect(compiled.querySelector('.minimize-button > img')?.getAttribute('src')).toBe('assets/icons/minimize-button.svg');
         });
+      });
+
+      describe('view', () => {
+        it('should exist by default', () => {
+          expect(compiled.querySelector('.view-button')).toBeTruthy();
+        });
+
+        // TODO test not existing when hideButtons set to true
 
         it('should have proper starting view button icon', () => {
           expect(compiled.querySelector('.view-button > img')?.getAttribute('src')).toBe('assets/icons/maximize-button.svg');
         });
+      });
 
-        // TODO add test for changing view button icon
-        // test presses button, tests for icon
+      describe('close', () => {
+        it('should exist by default', () => {
+          expect(compiled.querySelector('.close-button')).toBeTruthy();
+        });
+
+        // TODO test not existing when hideButtons set to true
 
         it('should have proper close button icon', () => {
           expect(compiled.querySelector('.close-button > img')?.getAttribute('src')).toBe('assets/icons/close-button.svg');
         });
       });
-
-      // describe('Hiding', () => {
-      // TODO modify
-      // it('should not display minimize button if hideButtons is true', () => {
-      //   const minimizeButton = compiled.querySelector('.minimize-button');
-      //   minimizeButton?.classList.add('hide-button');
-
-      //   expect(globalThis.getComputedStyle(minimizeButton as Element).display).toBe('none');
-      // });
-
-      // it('should not display view change button on .hide-button', () => {
-      //   const viewButton = compiled.querySelector('.view-button');
-      //   viewButton?.classList.add('hide-button');
-
-      //   expect(globalThis.getComputedStyle(viewButton as Element).display).toBe('none');
-      // });
-      // });
-
-      // TODO make something like this work
-      // describe("Alerts", () => {
-      //   it("should always be focused", () => {
-      //     component.alert = true;
-
-      //     expect(component.isElementFocused()).toBeTruthy();
-      //   })
-      // })
-
-      // TODO add functionality tests
-      // describe("Functionality", () => {
-      //   it("should minimize on button press", () => {
-
-      //   })
-
-      //   it("should close on button press", () => {
-
-      //   })
-      // })
     });
   });
 
