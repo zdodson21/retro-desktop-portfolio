cc6cf2bb5a809016a1a759c80e99d6338ef7fc47
Zach Dodson <zdodson21@gmail.com>
2025-11-11 19:02:44 -0500

    taskbar properties init tests

--- src/components/programs/taskbar-properties/taskbar-properties.component.spec.ts
+++ src/components/programs/taskbar-properties/taskbar-properties.component.spec.ts
@@ -5,6 +5,7 @@ import { TaskbarPropertiesComponent } from './taskbar-properties.component';
 describe('TaskbarPropertiesComponent', () => {
   let component: TaskbarPropertiesComponent;
   let fixture: ComponentFixture<TaskbarPropertiesComponent>;
+  let compiled: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,10 +15,31 @@ describe('TaskbarPropertiesComponent', () => {
 
     fixture = TestBed.createComponent(TaskbarPropertiesComponent);
     component = fixture.componentInstance;
+    compiled = fixture.nativeElement as HTMLElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a wrapper', () => {
+    expect(compiled.querySelector('.taskbar-properties-wrapper')).toBeTruthy();
+  });
+
+  describe('Structure', () => {
+    describe('Window Frame', () => {
+      it('should have proper window title', () => {
+        expect(compiled.querySelector('window-frame')?.getAttribute('window-title')).toBe('Taskbar Properties');
+      });
+
+      it('should have proper focus name', () => {
+        expect(compiled.querySelector('window-frame')?.getAttribute('focus-name')).toBe('taskbar-properties');
+      });
+    });
+
+    it('should have a frame contents wrapper', () => {
+      expect(compiled.querySelector('window-frame .frame-contents')).toBeTruthy();
+    });
   });
 });
