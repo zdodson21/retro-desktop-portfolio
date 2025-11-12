487c3f5cbe0d69ca066096e09232d3f9084749a8
Zach Dodson <zdodson21@gmail.com>
2025-11-11 19:07:34 -0500

    welcome program init tests

--- src/components/programs/welcome/welcome.component.spec.ts
+++ src/components/programs/welcome/welcome.component.spec.ts
@@ -5,6 +5,7 @@ import { WelcomeComponent } from './welcome.component';
 describe('WelcomeComponent', () => {
   let component: WelcomeComponent;
   let fixture: ComponentFixture<WelcomeComponent>;
+  let compiled: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,10 +15,33 @@ describe('WelcomeComponent', () => {
 
     fixture = TestBed.createComponent(WelcomeComponent);
     component = fixture.componentInstance;
+    compiled = fixture.nativeElement as HTMLElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be wrapped', () => {
+    expect(compiled.querySelector('.welcome-wrapper')).toBeTruthy();
+  });
+
+  describe('Structure', () => {
+    describe('Window Frame', () => {
+      it('should have proper window title', () => {
+        expect(compiled.querySelector('window-frame')?.getAttribute('window-title')).toBe('Welcome');
+      });
+
+      it('should have proper focus name', () => {
+        expect(compiled.querySelector('window-frame')?.getAttribute('focus-name')).toBe('welcome');
+      });
+
+      // TODO test for proper percent width, height, and hide buttons true
+    });
+
+    it('should have a frame contents wrapper', () => {
+      expect(compiled.querySelector('window-frame .frame-contents')).toBeTruthy();
+    });
   });
 });
