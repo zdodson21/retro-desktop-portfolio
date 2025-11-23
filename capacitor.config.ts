3467cb180f41273bab5064bafbf8151a64e503ed
Zach Dodson <zdodson21@gmail.com>
2025-11-22 22:08:03 -0500

    fixed navbar and status bar issues

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'me.zachdodson.app',
   appName: "Zach Dodson's Portfolio",
-  webDir: 'dist/retro-desktop-portfolio/browser'
+  webDir: 'dist/retro-desktop-portfolio/browser',
+  plugins: {
+    "EdgeToEdge": {
+      "backgroundColor": "#000000"
+    }
+  }
 };
 
 export default config;
