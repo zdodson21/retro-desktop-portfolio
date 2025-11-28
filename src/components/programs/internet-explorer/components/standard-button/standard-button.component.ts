51207ea8ee9753d88708b476bc999cebae4ff482
Zach Dodson <zdodson21@gmail.com>
2025-11-28 12:25:38 -0500

    add hints to standard buttons

--- src/components/programs/internet-explorer/components/standard-button/standard-button.component.ts
+++ src/components/programs/internet-explorer/components/standard-button/standard-button.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { InternetExplorerService } from '../../internet-explorer.service';
 
 @Component({
   selector: 'standard-button',
@@ -8,8 +9,13 @@ import { Component, Input } from '@angular/core';
 })
 export class StandardButtonComponent {
   @Input({ alias: 'button-name', required: true }) public buttonName: string;
+  @Input({ alias: 'button-hint', required: true }) public buttonHint: string;
   protected text: string;
   protected hovered: boolean = false;
+
+  protected IEService: InternetExplorerService = inject(InternetExplorerService);
+  protected prevIcon: string;
+  protected prevValue: string;
 
   ngAfterContentInit() {
     let firstChar: string = this.buttonName?.charAt(0);
@@ -18,5 +24,41 @@ export class StandardButtonComponent {
     firstChar = firstChar?.toUpperCase();
 
     this.text = `${firstChar}${restOfString}`;
+  }
+
+  protected hover(): void  {
+    this.hovered = true;
+
+    if (this.IEService.statusBarIcon() !== 'assets/icons/windows-help.svg'){
+      this.prevIcon = this.IEService.statusBarIcon();
+    }
+
+    if (this.IEService.statusBarContent() !== this.buttonHint) {
+      this.prevValue = this.IEService.statusBarContent();
+    }
+
+    console.log(this.prevIcon);
+
+    this.IEService.statusBarIcon.set('assets/icons/windows-help.svg')
+    this.IEService.statusBarContent.set(this.buttonHint);
+  }
+
+  protected unhover(): void {
+    this.hovered = false;
+
+    if (this.prevIcon === 'assets/icons/internet-explorer/copy-button.svg') {
+      this.IEService.statusBarIcon.set('assets/icons/html-file.svg');
+    } else {
+      this.IEService.statusBarIcon.set(this.prevIcon);
+    }
+
+    if (this.prevValue === '' || this.prevValue === this.IEService.copyText) {
+      this.IEService.statusBarContent.set('Ready');
+    } else {
+      this.IEService.statusBarContent.set(this.prevValue);
+    }
+
+    this.prevIcon = '';
+    this.prevValue = '';
   }
 }
