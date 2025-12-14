70755a7b3ca6303b563a2ed113d4f509f2e56103
Zach Dodson <zdodson21@gmail.com>
2025-12-14 09:27:58 -0500

    wasm prep

--- src/components/programs/calculator/calculator.component.spec.ts
+++ src/components/programs/calculator/calculator.component.spec.ts
@@ -1,4 +1,5 @@
 import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting, HttpTestingController } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
 import { CalculatorComponent } from './calculator.component';
@@ -7,16 +8,18 @@ describe('CalculatorComponent', () => {
   let component: CalculatorComponent;
   let fixture: ComponentFixture<CalculatorComponent>;
   let compiled: HTMLElement;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [CalculatorComponent],
-      providers: [provideRouter([]), provideHttpClient()],
+      providers: [provideRouter([]), provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(CalculatorComponent);
     component = fixture.componentInstance;
     compiled = fixture.nativeElement as HTMLElement;
+    httpTestingController = TestBed.inject(HttpTestingController);
     fixture.detectChanges();
   });
 
