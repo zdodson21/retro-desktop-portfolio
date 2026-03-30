9e4d60b35c836eed2cbb7165408a712e27c12054
Zach Dodson <zdodson21@gmail.com>
2026-03-30 13:25:37 -0400

    create router service

---
+++ src/services/router/router.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RouterService {
+  // TODO add all common router usages into functions here. Use interfaces to create objects for any inputs.
+}
